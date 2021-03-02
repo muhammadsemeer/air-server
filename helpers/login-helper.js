56a44716d4d3936443289b0da29f2d006c90b23a
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-03-02 16:53:41 +0530

    db.get() added instead of USER model

--- helpers/login-helper.js
+++ helpers/login-helper.js
@@ -1,28 +1,36 @@
-const USER = require("../models/user");
+const db = require("../config/connection");
+const collection = require("../config/collection");
 const bcrypt = require("bcrypt");
 
 module.exports = {
   doSignup: (data) => {
     return new Promise(async (resolve, reject) => {
       try {
-        let userExist = USER.findOne({
-          $and: [
-            { email: data.email },
-            { $or: [{ status: "active" }, { status: "blocked" }] },
-          ],
-        });
+        let userExist = await db
+          .get()
+          .collection(collection.USER_COLLECTION)
+          .findOne({
+            $and: [
+              { email: data.email },
+              { $or: [{ status: "active" }, { status: "blocked" }] },
+            ],
+          });
         if (userExist && userExist.status === "blocked") {
           reject({ msg: "Your account is temporarily disabled for 30 Days" });
         } else if (userExist) {
           reject({ msg: "User Already Registered" });
         } else {
-          data.password = await bcrypt.hash(data.password, 10);
-          USER.insertOne(data).then((data) => {
-            resolve(data.ops[0]);
-          });
+          data.password = await bcrypt.hash(d, 10);
+          data.status = "active"
+          db.get()
+            .collection(collection.USER_COLLECTION)
+            .insertOne(data)
+            .then((data) => {
+              resolve(data.ops[0]);
+            });
         }
       } catch (error) {
-        resolve({ code: 500 });
+        reject({ code: 500 });
       }
     });
   },
