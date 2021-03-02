ea99108d4bfa36d21bf44664ca275cc9891ee2f6
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-03-02 16:52:23 +0530

    models removed

--- models/user.js
+++
@@ -1,5 +0,0 @@
-const { connect } = require("../config/connection");
-
-const db = require("../config/connection");
-const collection = require("../config/collection");
-module.exports = db.get(collection.USER_COLLECTION);
