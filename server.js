bc382fa08525524c0f96a0e9d687b83cd828c393
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-03-02 14:51:22 +0530

    added logger

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const db = require("./config/connection");
-
+const logger = require("morgan");
 // Server Port Setting
 const port = process.env.PORT || 3001;
 app.set("port", port);
@@ -11,11 +11,14 @@ app.set("port", port);
 // Create Http Server
 const server = http.createServer(app);
 
+// Middilewares
+app.use(logger("dev"));
+
 // Database Connetcion
 db.connect((error) => {
-  if (error) throw error
+  if (error) throw error;
   console.log("Database Connected");
-})
+});
 
 // Server Listen
 server.listen(port);
